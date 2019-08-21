538a9ffde382bef7160aee331cda38084ff0bbb9
Philip Loa <boyet007@gmail.com>
2019-08-21 15:09:03 +0700

    09 - Intro to Hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import Navbar from './components/Navbar'
-import BookList from './components/BookList'
-import ThemeContextProvider from './context/ThemeContext'
-import ThemeToggle from './components/ThemeToggle'
-import AuthContextProvider from './context/AuthContext'
+import SongList from './components/SongList'
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookList />
-        </AuthContextProvider>
-        <ThemeToggle />
-      </ThemeContextProvider>
+      <SongList />
     </div>
-  )
+  );
 }
 
 export default App;
