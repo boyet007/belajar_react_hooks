aa60f55ee339047bdc295d2ec723fb139799a83e
Philip Loa <boyet007@gmail.com>
2019-08-21 11:14:03 +0700

    01 - Introductions

---
+++ src/components/BookList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+    render() {
+        return (
+            <div>
+                <div className="book-list">
+                    <ul>
+                        <li>the way of kings</li>
+                        <li>the name of the winds</li>
+                        <li>the final empoire</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BookList
